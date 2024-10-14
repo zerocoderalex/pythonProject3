d6c80d0c6588a02fc78e0f15c0c95f74b9371ac5
zerocoderalex <alex12449@mail.ru>
2024-10-15 00:12:23 +0500

    new code

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_english_words():
+    url = "https://randomword.com/"
+    try:
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        english_words = soup.find("div", id="random_word").text.strip()
+        word_definition = soup.find("div", id="random_word_definition").text.strip()
+
+        return {
+            "english_words": english_words,
+            "word_definition": word_definition
+        }
+    except:
+        print("Произошла ошибка")
+
+def word_game():
+    print("Добро пожаловать в игру!")
+    while True:
+        word_dict = get_english_words()
+        word = word_dict.get("english_words")
+        word_definition = word_dict.get("word_definition")
+
+        print(f"Значение слова - {word_definition}")
+        user = input("Что это за слово? ")
+        if user == word:
+            print("Все верно!")
+        else:
+            print(f"Ответ неверный, правильное слово -{word}")
+        play_again = input("Хотите сыграть еще? y/n")
+        if play_again != "y":
+            print("Спасибо за игру!")
+            break
+word_game()
