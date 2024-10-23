24711fae783e0e0000774529187afb100d0e7f94
zerocoderalex <alex12449@mail.ru>
2024-10-24 00:23:31 +0500

    add midlsalary

--- part1.py
+++ part1.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv('animal.csv')
+# df = pd.read_csv('animal.csv')
 # print(df.head())
 # print(df.info())
-print(df.describe())
+# print(df.describe())
+
+df = pd.read_csv('dz.csv')
+
+df.fillna(value=0, inplace=True)
+midlsalary = df.groupby('City')['Salary'].mean()
+
+print(midlsalary)
 
